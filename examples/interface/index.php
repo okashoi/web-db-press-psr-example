636625a6777c7972dc11d9b1f7cef0db4fe42338
Okada Shohei <okashoicircus0@gmail.com>
2020-06-08 05:34:55 +0900

    ロガーを Monolog\Logger に置き換え

--- examples/interface/index.php
+++ examples/interface/index.php
@@ -2,11 +2,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use Willgate\Log\Logger;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use Willgate\SomethingProcessor;
 
 // 同じディレクトリのapp.logファイルにログ出力
-$logger = new Logger(__DIR__ . '/app.log');
+$handler = new StreamHandler(__DIR__ . '/app.log');
+$logger = new Logger('my_logger');
+$logger->pushHandler($handler);
 $processor = new SomethingProcessor($logger);
 
 $processor->process();
